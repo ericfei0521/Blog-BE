69e4b7a811e91a90ccfa382fa20ff21521dc6e5f
ericfei306 <eric0521bigjohn@gmail.com>
2024-06-07 22:47:36 +0800

    init

---
+++ controllers/posts.js
@@ -0,0 +1,17 @@
+exports.getPosts = (req, res, next) => {
+    res.status(200).json({
+        posts: [{ title: 'firstPost', content: 'test post' }],
+    });
+};
+exports.createPost = (req, res, next) => {
+    const { title, content } = req.body;
+    //update to db
+    res.status(201).json({
+        message: 'create success',
+        post: {
+            id: new Date().toISOString(),
+            title: title,
+            content: content,
+        },
+    });
+};
